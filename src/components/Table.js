47d90d6af23d252e03fc1af0c59c02654896b25a
Christian Mendes Pereira <christianp3m@gmail.com>
2022-04-02 15:50:56 -0300

    Requirement 8 done

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
       <div>
         <table>
@@ -16,10 +19,56 @@ class Table extends Component {
             <th>Moeda de conversão</th>
             <th>Editar/Excluir</th>
           </tr>
+          {
+            expenses.map(({
+              description,
+              tag,
+              method,
+              value,
+              currency,
+              exchangeRates,
+              id,
+            }) => {
+              const { name, ask } = exchangeRates[currency];
+              return (
+                <tr key={ id }>
+                  <td>{description}</td>
+                  <td>{tag}</td>
+                  <td>{method}</td>
+                  <td>{Number(value).toFixed(2).toString()}</td>
+                  <td>{currency}</td>
+                  <td>{name}</td>
+                  <td>{Number(ask).toFixed(2).toString()}</td>
+                  <td>{Number(value * ask).toFixed(2).toString()}</td>
+                  <td>Real</td>
+                  <td>
+                    <button type="button">Editar</button>
+                    <button type="button">Excluir</button>
+                  </td>
+                </tr>
+              );
+            })
+          }
         </table>
       </div>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number,
+    value: PropTypes.number,
+    description: PropTypes.string,
+    currency: PropTypes.string,
+    method: PropTypes.string,
+    tag: PropTypes.string,
+    exchangeRates: PropTypes.objectOf(PropTypes.any),
+  })).isRequired,
+};
+
+export default connect(mapStateToProps)(Table);

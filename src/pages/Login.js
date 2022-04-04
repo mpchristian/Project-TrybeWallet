ab10772ee7fa19bc2e8c4ce328af1c1b22ad1ba0
Christian Mendes Pereira <christianp3m@gmail.com>
2022-04-04 18:16:48 -0300

    Requirement 10 done - without css error

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { actionUser } from '../actions';
-import './Login.css';
 
 class Login extends React.Component {
   constructor(props) {

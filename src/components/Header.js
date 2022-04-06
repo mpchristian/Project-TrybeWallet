82145843ba37b6ab0a88f2dd0701a535e757d6d9
Christian Mendes Pereira <christianp3m@gmail.com>
2022-04-06 01:30:02 -0300

    Styling header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,24 +6,29 @@ class Header extends Component {
     const { email, allValues } = this.props;
     const total = allValues.reduce((acc, curr) => acc + curr, 0);
     return (
-      <div>
+      <header className="header-container">
         <div>
-          <h1>TrybeWallet</h1>
+          <span id="emoji-header" role="img" aria-label="Money flying">💸</span>
         </div>
-        <div>
-          <p data-testid="email-field">{ email }</p>
+        <div className="header-right">
+          <div>
+            <p data-testid="email-field">{ email }</p>
+          </div>
+          <div className="total-field">
+            <div>
+              Total:
+              {' '}
+              <span data-testid="total-field">
+                { total.toFixed(2) }
+              </span>
+              <span data-testid="header-currency-field">
+                {' '}
+                BRL
+              </span>
+            </div>
+          </div>
         </div>
-        <div>
-          <p data-testid="header-currency-field">BRL</p>
-        </div>
-        <div>
-          <p>
-            Total:
-            {' '}
-            <span data-testid="total-field">{ total.toFixed(2) }</span>
-          </p>
-        </div>
-      </div>
+      </header>
     );
   }
 }
